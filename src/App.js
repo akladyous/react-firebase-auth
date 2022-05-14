daecf888949d4ec3dc93b8c94d3d856808514a12
Boula Akladyous <akladyous@gmail.com>
2022-05-14 18:52:51 -0400

    added userContext to main route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./ui/Header.js";
+import Home from "./ui/Home.js";
+import UsersHome from "./users/UsersHome.js";
+import UsersLogin from "./users/UsersLogin.js";
+import UsersLogout from "./users/UsersLogout.js";
+import UsersSignup from "./users/UsersSignup.js";
+import { AuthProvider } from "./hooks/useAuth.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+
+                    <Route path="users" element={<UsersHome />}>
+                        <Route path="login" element={<UsersLogin />} />
+                        <Route path="logout" element={<UsersLogout />} />
+                        <Route path="signup" element={<UsersSignup />} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </>
+    );
 }
 
 export default App;
