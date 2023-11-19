f217985ca7fe3bc5e93ab8cbcce96b6b9f10d445
Boula Akladyous <akladyous@gmail.com>
2023-11-19 16:15:17 -0500

    Untracked files on react-firebase-auth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import Header from "./ui/Header.js";
-import Home from "./ui/Home.js";
-import UsersHome from "./users/UsersHome.js";
-import UsersLogin from "./users/UsersLogin.js";
-import UsersLogout from "./users/UsersLogout.js";
-import UsersSignup from "./users/UsersSignup.js";
-import { AuthProvider } from "./hooks/useAuth.js";
+import { Routes, Route } from 'react-router-dom';
+import Header from './ui/Header.js';
+import Home from './ui/Home.js';
+import UsersHome from './users/UsersHome.js';
+import UsersLogin from './users/UsersLogin.js';
+import UsersLogout from './users/UsersLogout.js';
+import UsersSignup from './users/UsersSignup.js';
+import { AuthProvider } from './hooks/useAuth.js';
 
 function App() {
-    return (
-        <>
-            <Header />
-            <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<Home />} />
+  return (
+    <>
+      <Header />
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
 
-                    <Route path="users" element={<UsersHome />}>
-                        <Route path="login" element={<UsersLogin />} />
-                        <Route path="logout" element={<UsersLogout />} />
-                        <Route path="signup" element={<UsersSignup />} />
-                    </Route>
-                </Routes>
-            </AuthProvider>
-        </>
-    );
+          <Route path='users' element={<UsersHome />}>
+            <Route path='login' element={<UsersLogin />} />
+            <Route path='logout' element={<UsersLogout />} />
+            <Route path='signup' element={<UsersSignup />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </>
+  );
 }
 
 export default App;
